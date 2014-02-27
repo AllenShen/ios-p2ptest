e8572fe078781ab4a721f2cba4fad6e69cc83b33
沈冬冬 <sddhbk@gmail.com>
2014-02-27 15:48:00 +0800

    no message

--- ios-p2ptest/Classes/p2pnetwork/P2PConnectManager.cpp
+++ ios-p2ptest/Classes/p2pnetwork/P2PConnectManager.cpp
@@ -48,7 +48,7 @@ void P2PConnectManager::initInfo() {
     natPunchThroughHandler = new NatPunchThroughHandler();
     proxyHandler = new UDPProxyHandler();
 
-    this->peerGuid.FromString("357633825");
+    this->peerGuid.FromString("18446744073474235218");
     this->isHost = true;
 
     enterStage(P2PStage_Initial, NULL);
