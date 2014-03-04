f767dca0a723d4d4f64d3ca80c4effe26f3537ef
AllenShen <sddhbk@gmail.com>
2014-03-04 19:54:48 +0800

    no message

--- ios-p2ptest/Classes/p2pnetwork/P2PConnectManager.cpp
+++ ios-p2ptest/Classes/p2pnetwork/P2PConnectManager.cpp
@@ -51,7 +51,7 @@ void P2PConnectManager::initInfo() {
     proxyHandler = new UDPProxyHandler();
 
     this->peerGuid.FromString("18446744073508371605");
-    this->isHost = true;
+    this->isHost = false;
 
     enterStage(P2PStage_Initial, NULL);
 
